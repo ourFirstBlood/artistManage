966a3c2cd857f98dd391d09ea8c416a4b7dbf9fe
liangjx <15766273878@163.com>
2019-12-05 01:28:28 +0800

    debug

--- pc/node/mysql/index.js
+++ pc/node/mysql/index.js
@@ -8,7 +8,7 @@ colors.setTheme({
 var mysql_config = {
   host: 'localhost',
   user: 'root',
-  password: 'root', //本地是root
+  password: '39547', //本地是root
   port: '3306',
   database: 'artist'
 }
